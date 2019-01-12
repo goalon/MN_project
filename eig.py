c162a05ea774a47f4265376efe06f86ed9707535
Mateusz Bajorek <mb385122@students.mimuw.edu.pl>
2019-01-12 18:13:13 +0100

    Renames.

--- eig.py
+++ eig.py
@@ -4,18 +4,22 @@ from numpy.random import rand
 from numpy.linalg import norm
 
 
-def power_iteration(A: array, iterations: int = 1000) -> Tuple[array, float]:
-    v = rand(A.shape[1])
+eps = 10**(-6)
 
-    for _ in range(iterations):
-        v_next = A @ v
-        v = v_next / norm(v_next)
 
-    return v, ((A @ v).T @ v) / (v.T @ v)
+def eigenpair(A: array) -> Tuple[array, float]:
+    eigvec = rand(A.shape[1])
+    eigval = ((A @ eigvec).T @ eigvec) / (eigvec.T @ eigvec)
+
+    while norm(A @ eigvec - eigval @ eigvec) > eps:
+        eigvec_next = A @ eigvec
+        v = eigvec_next / norm(eigvec_next)
+
+    return eigvec, eigval
 
 
 A = array([[1, 2], [3, 4]])
-eig_v, eig_n = power_iteration(A)
+eig_v, eig_n = eigenpair(A)
 B = A - eig_n * identity(A.shape[1])
-eig_v_2, eig_n_2 = power_iteration(B)
+eig_v_2, eig_n_2 = eigenpair(B)
 print(eig_v_2, eig_n_2 + eig_n)
